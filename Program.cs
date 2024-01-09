b181cd05c8ef502fc460311eabcbbe6a4e649a33
LucasNekoo <lucasjsp12@gmail.com>
2024-01-09 03:05:13 -0300

    Desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,24 @@
-﻿using DesafioPOO.Models;
+﻿using System;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nokia nokia = new Nokia("123456789", "Nokia Modelo XYZ", "IMEI123", 64);
+            nokia.Ligar();
+            nokia.ReceberLigacao();
+            nokia.InstalarAplicativo("WhatsApp");
+
+            Console.WriteLine();
+
+            Iphone iphone = new Iphone("987654321", "iPhone Modelo ABC", "IMEI456", 128);
+            iphone.Ligar();
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("Instagram");
+
+            Console.ReadLine();
+        }
+    }
+}
